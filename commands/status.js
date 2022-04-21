4bcc85a0481038b614f7a69e63b24740dc228fcf
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2022-04-21 17:05:23 +0300

    Fix formatting

--- commands/status.js
+++ commands/status.js
@@ -12,7 +12,7 @@ module.exports = {
         const output = `User: <code>${os.userInfo().username}@${os.hostname}</code>\n` +
             `System uptime: <code>${humanize(os.uptime * 1e3, { round: true })}</code>\n` +
             `Process uptime: <code>${humanize(process.uptime() * 1e3, { round: true })}</code>\n` +
-            (process.platform != "win32" ? `Load average: \`${os.loadavg().join(", ")}\`\n` : "") +
+            (process.platform != "win32" ? `Load average: <code>${os.loadavg().join(", ")}</code>\n` : "") +
             `Node version: <code>${process.version}</code>`
 
         ctx.reply(output, {
